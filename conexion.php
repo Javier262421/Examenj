9e45f784c68c517c33650e066319c4a5dcbb1d38
Javier262421 <javiermeza8319@gmail.com>
2026-09-23 14:16:05 +0200

    Segundo Commit

---
+++ conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+$conexion = new mysqli(
+
+    "localhost",
+
+    "root",
+
+    "",
+
+    "cine"
+
+);
+
+if ($conexion->connect_error) {
+
+    die("Error de conexión: " . $conexion->connect_error);
+
+}
+
+$conexion->set_charset("utf8mb4");
+
+
+
+
